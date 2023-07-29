1c2773a9aa57d364e0f8a09c55937d2020b68617
Jose Rodriguez <jrodriguez@frontporchsolutions.com>
2023-07-28 22:17:59 -0500

    useContext

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,4 +1,10 @@
-function TaskCard({ task, deleteTask }) {
+import { useContext } from "react";
+import { TaskContext } from "../context/TaskContext";
+
+function TaskCard({ task}) {
+
+	const {deleteTask} = useContext(TaskContext)
+
 	return (
 		<div>
 			<h1>{task.title}</h1>
