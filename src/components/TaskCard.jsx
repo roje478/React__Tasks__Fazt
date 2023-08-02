bc00e7e7656fac9fa49fe950c530067a134d3d16
Jose Eduardo R <jrodriguez@frontporchsolutions.com>
2023-08-02 16:18:43 -0500

    adjustment in styles for Taskcard

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -5,11 +5,13 @@ function TaskCard({ task }) {
   const { deleteTask } = useContext(TaskContext);
 
   return (
-    <div className="bg-gray-800 text-white p-4 rounded-md">
-      <h1 className="text-xl font-bold capitalize">{task.title}</h1>
-      <p className="text-gray-500 text-sm">{task.description}</p>
+    <div className="bg-gray-800 text-white p-6 rounded-md flex lg:flex-col items-start justify-between">
+      <div className="w-auto lg:w-full flex flex-wrap flex-col items-start justify-start">
+        <h1 className="text-xl font-bold capitalize">{task.title}</h1>
+        <p className="text-gray-500 text-sm">{task.description}</p>
+      </div>
       <button
-        className="bg-red-500 px-2 py-1 rounded-md mt-4 hover:bg-red-400"
+        className="bg-red-500 px-2 py-1 rounded-md mt-4 sm:mt-1 lg:mt-4 hover:bg-red-400"
         onClick={() => deleteTask(task.id)}
       >
         Eliminar tarea
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -11,7 +11,7 @@ function TaskList() {
 	}
 
 	return (
-		<div className="grid grid-cols-4 gap-4">
+		<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
 			{tasks.map((task) => (
 				<TaskCard key={task.id} task={task} />
 			))}
